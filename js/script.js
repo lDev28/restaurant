0421c2655867f82905e29b4e76769eda8c1e1740
lDev28 <arslan_28.03@inbox.ru>
2023-01-13 22:25:37 +0500

    correct script

--- js/script.js
+++ js/script.js
@@ -42,71 +42,145 @@ for (let i = 0; i < coloredTitle.length; i++) {
 	titleElem.innerHTML = readyTitle
 }
 
+
+
+// MENU BURGER
 const burgerMenu = d.getElementById('burgerMenu')
 
-burgerMenu.addEventListener('click', () => {
+
+const burgerStatus = {
+	opened: false
+}
+burgerMenu.addEventListener('click', setBurgerFunc)
+function setBurgerFunc() {
+	const headerMenu = d.querySelector('.header__menu')
 	d.querySelector('.header__menu').classList.toggle('active')
 	d.querySelector('.header__burger').classList.toggle('active')
 	d.querySelector('.header__burger-elem').classList.toggle('active')
-	// d.querySelector('.wrapper').classList.toggle('active')
+	if (headerMenu.className == 'header__menu active') {
+		burgerStatus.opened = true
+	} else {
+		burgerStatus.opened = false
+	}
+
+	d.getElementById('btnTop').classList.toggle('active')
+}
+
+
+
+
+// SCROLL BTN
+
+const btnTop = d.getElementById('btnTop')
+
+btnTop.addEventListener('click', () => {
+
+	wrapperBlock.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	})
 })
 
-// console.log(wrapperBlock);
 
+wrapperBlock.addEventListener('scroll', () => {
+	if (wrapperBlock.scrollTop > 100) {
+		btnTop.style.display = 'block'
+		btnTop.style.opacity = '100%'
+	} else {
+		btnTop.style.display = 'none'
+	}
+})
+
+
+// SCROLL NAV
+
+
+wrapperBlock.addEventListener('scroll', () => {
+
+	let wrapperHigth = wrapperBlock.scrollHeight
+
+	const wrapperTotalHight = wrapperBlock.scrollTop
+
+	const scrollNav = d.querySelector('.scroll-nav')
+
+	let scrollNavHight = (wrapperTotalHight * 100) / wrapperHigth
+
+	scrollNav.style.width = `${scrollNavHight + 12}%`
+})
+
+
+const menuLink = d.querySelector('#menuLink')
+const menuSection = d.querySelector('.menu')
+menuLink.addEventListener('click', () => {
+	setScroll()
+	setBurgerFunc()
+})
+
+function setScroll() {
+	const topOffset = d.querySelector('.menu').offsetHeight * 1.6;
+	const elementPosition = menuLink.getBoundingClientRect().top;
+	const offsetPosition = elementPosition - topOffset;
+	const scrollCoordinate = offsetPosition + (offsetPosition * -1) * 2
+	wrapperBlock.scrollBy({
+		top: scrollCoordinate,
+		behavior: 'smooth'
+	});
+}
+
+// console.log(wrapperHigth);
 
 
 // LOADER
-const images = d.querySelectorAll('img')
+// const images = d.querySelectorAll('img')
 
-for (const image of images) {
-	image.addEventListener('load', loaded)
-}
+// for (const image of images) {
+// 	image.addEventListener('load', loaded)
+// }
 
-function loaded() {
-	const loader = d.querySelector('.loader')
-	loader.style.display = 'none'
-}
+// function loaded() {
+// 	const loader = d.querySelector('.loader')
+// 	loader.style.display = 'none'
+// }
 
 
 // SLIDER
-const container = d.querySelector('.container')
-const containerWidth = container.clientWidth
-const menuSliderTrack = d.querySelector('.menu-slider__track')
 
-const menuSliderItem = d.querySelectorAll('.menu-slider__item')
+let viewport = d.getElementById("menuSlider").offsetWidth;
+let btnNext = d.getElementById("menuSliderNextBtn");
+let btnPrev = d.getElementById("menuSliderPrevBtn");
+let slider = d.querySelector(".menu-slider__track");
+let viewSliders = d.querySelectorAll(".menu-slider__viewSlide");
+let viewSlide = 0;
 
-const countItem = 8
-const countShowItem = 3
-const itemToScroll = 2
-let position = 0
+viewSliders[0].classList.add('active')
 
-const menuSliderNextBtn = d.getElementById('menuSliderNextBtn')
-const menuSliderPrevBtn = d.getElementById('menuSliderPrevBtn')
 
-const menuSliderItemWidth = containerWidth / countShowItem
-const scrollCount = menuSliderItemWidth * itemToScroll
+btnNext.addEventListener("click", nextBtnFunc);
 
-for (const item of menuSliderItem) {
-	const menuTrackWidth = countItem * item.clientWidth + 'px'
-	item.style.flexBasis = menuSliderItemWidth + 'px'
-}
-menuSliderTrack.style.width = containerWidth + 'px'
-
-menuSliderNextBtn.addEventListener('click', () => {
-	position += scrollCount
-	console.log(position);
-
-	if (position == 0) {
-		menuSliderNextBtn.removeEventListener
+function nextBtnFunc() {
+	viewSliders[viewSlide].classList.remove('active')
+	if (viewSlide < 7) {
+		viewSlide++;
+	} else {
+		viewSlide = 0;
 	}
+	viewSliders[viewSlide].classList.add('active')
+	slider.style.left = -viewSlide * viewport + "px";
 
-	menuSliderTrack.style.transform = `translateX(-${position}px)`
-})
+}
+
+setInterval(nextBtnFunc, 3000);
 
 
-menuSliderPrevBtn.addEventListener('click', () => {
-	position -= scrollCount
-	console.log(position);
+btnPrev.addEventListener("click", function () {
+	viewSliders[viewSlide].classList.remove('active')
+	if (viewSlide > 0) {
+		viewSlide--;
+	} else {
+		viewSlide = 7;
+	}
+	viewSliders[viewSlide].classList.add('active')
+	slider.style.left = -viewSlide * viewport + "px";
+});
 
-	menuSliderTrack.style.transform = `translateX(-${position}px)`
-})
+
